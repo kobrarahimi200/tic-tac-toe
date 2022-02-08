8d868db34a52fe1305011fee4bb08aac7a251c1b
kobra <kobrarahimi2021@gmail.com>
2022-02-08 19:47:13 +0100

    completed game with two human player.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,26 +10,53 @@ import { Gamelogic } from '../gamelogic';
 
 export class GameComponent implements OnInit {
 
+  currentPlayer: string = "";
+
   constructor(public game: Gamelogic) { }
 
   ngOnInit(): void {
   }
-  
+
   startGame(): void {
     this.game.gameStart();
-    const cuurentPlayer = 'Current turn: Player' + this.game.currTurn;
-    const information = document.querySelector('.current-status');
-    information.innerHTML = cuurentPlayer;
+    this.currentPlayer = 'Current turn: Player' + this.game.currTurn;
+    const information = <HTMLParagraphElement>document.querySelector('.current-status');
+    information.innerHTML = this.currentPlayer;
   }
 
- //promise is used to call the async functions.
+  //promise is used to call the async functions.
   async clickSubfield(subfield: any): Promise<void> {
+
     if (this.game.gameStatus === 1) {
+      const information = <HTMLParagraphElement>document.querySelector('.current-status');
       const position = subfield.currentTarget.getAttribute('position');
-      console.log("pos " + position);
-      if(this.game.currTurn ==0){
-        position
+
+      this.game.setField(position, this.game.currTurn);
+      const color = this.game.getPlayerColorClass();
+      subfield.currentTarget.classList.add(color);
+
+      await this.game.checkWinner().then((end: boolean) => {
+        if (this.game.gameStatus === 0 && end) {
+          information.innerHTML = "winner is player " + this.game.currTurn;
+        }
+      });
+      // await this.game.checkWinnner(); //why use await : becuase we should wait until get the result of this function and
+      //when we dont have any winner then go the the next func and check if the field is full or not.
+      await this.game.checkisFieldFull().then((end: boolean) => {
+        if (this.game.gameStatus === 0 && end) {
+          information.innerHTML = "no Winner";
+        }
+      });
+
+      this.game.changePlyaer();
+
+      //update the current player status
+      if (this.game.gameStatus === 1) {
+        this.currentPlayer = 'Current turn: Player' + this.game.currTurn;
+
+        information.innerHTML = this.currentPlayer;
       }
     }
+
   }
 }
--- src/app/gamelogic.ts
+++ src/app/gamelogic.ts
@@ -1,24 +1,121 @@
-import {Status} from'./status';
+import { Status } from './status';
 
 
 export class Gamelogic {
 
-    gameField: Array<number> =[];
-    currTurn : number;
+    gameField: Array<number> = [];
+    currTurn: number;
     gameStatus: Status;
+    winSituationOne: Array<Array<number>> = [
+        [1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [1, 0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0, 1],
+        [0, 0, 1, 0, 1, 0, 1, 0, 0],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1],
 
-    public constructor(){
-        this.currTurn =0;
+    ]
+    winSituationTwo: Array<Array<number>> = [
+        [2, 2, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 2, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 2, 2, 2],
+        [2, 0, 0, 2, 0, 0, 2, 0, 0],
+        [0, 2, 0, 0, 2, 0, 0, 2, 0],
+        [0, 0, 2, 0, 0, 2, 0, 0, 2],
+        [0, 0, 2, 0, 2, 0, 2, 0, 0],
+        [2, 0, 0, 0, 2, 0, 0, 0, 2],
+
+    ]
+
+    public constructor() {
+        this.currTurn = 0;
         this.gameStatus = Status.STOP;
-        this.gameField = [0,0,0,0,0,0,0,0,0];
+        this.gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
-    gameStart(){
-        this.gameStatus =Status.START;
-        this.gameField =[0,0,0,0,0,0,0,0,0];
+    gameStart(): void {
+        this.gameStatus = Status.START;
+        this.gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         this.currTurn = this.randomPlayerStart();
     }
-    randomPlayerStart(): number{
-        const startPlayer = Math.floor(Math.random() *2 ) + 1;
-        return startPlayer; 
+    randomPlayerStart(): number {
+        const startPlayer = Math.floor(Math.random() * 2) + 1;
+        return startPlayer;
+    }
+    setField(position: number, value: number): void {
+        this.gameField[position] = value;
+    }
+    getPlayerColorClass(): string {
+        const colorClass = (this.currTurn === 2) ? 'player-two' : 'player-one';
+        return colorClass;
+    }
+    /**
+     * change the current player 
+     */
+    changePlyaer(): void {
+        this.currTurn = (this.currTurn === 2) ? 1 : 2;
+    }
+    /**
+     * checks if the given arrays are eqaul
+     * @param a first given array
+     * @param b second given array
+     * @returns true if the arrays are equal
+     */
+    arrayEquals(a: Array<any>, b: Array<any>): boolean {
+        return Array.isArray(a) && Array.isArray(b) && a.length === b.length &&
+            a.every((value, index) => value === b[index]); //TODO read about it
+    }
+    /**
+     * checks winner
+     * he return type of an async function
+     *  or method must be the global Promise<T> type.
+     */
+    async checkWinner(): Promise<boolean> {
+        let isWinner = false;
+        const checckArry = (this.currTurn === 1) ? this.winSituationOne : this.winSituationTwo;
+        const currentArray: any = [];
+
+        this.gameField.forEach((subfield, index) => {
+            if (subfield !== this.currTurn) {
+                currentArray[index] = 0;
+            } else {
+                currentArray[index] = subfield;
+            }
+
+            checckArry.forEach((checkfield, checkindex) => {
+                if (this.arrayEquals(checkfield, currentArray)) {
+                    isWinner = true;
+                }
+            })
+        });
+
+        if (isWinner) {
+            this.gameEnd();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    /**
+     * checks if the field is full or not
+     */
+    async checkisFieldFull(): Promise<boolean> {
+        let isFull = true;
+        if (this.gameField.includes(0)) {
+            isFull = false;
+        }
+        if (isFull) {
+            this.gameEnd();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    gameEnd(): void {
+        this.gameStatus = Status.STOP;
     }
 }
